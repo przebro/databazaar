4d9d50355aa0d076f1e976613a7b7658b0ee96ba
przebro <pb.secacc@gmail.com>
2023-03-19 23:08:17 +0100

    collection exists

--- store/store.go
+++ store/store.go
@@ -16,15 +16,16 @@ func RegisterStoreFactory(name string, initfunc initializerFunc) {
 	storeInitializer[name] = initfunc
 }
 
-//DataStore - common interface that represents a database
+// DataStore - common interface that represents a database
 type DataStore interface {
 	CreateCollection(ctx context.Context, name string) (collection.DataCollection, error)
 	Collection(context.Context, string) (collection.DataCollection, error)
 	Status(context.Context) (string, error)
+	CollectionExists(ctx context.Context, name string) bool
 	Close(ctx context.Context)
 }
 
-//NewStore - Creates a new store
+// NewStore - Creates a new store
 func NewStore(connection string) (DataStore, error) {
 
 	var initStoreFunc initializerFunc
